caed3ea5e596c28b5ebbef14ab80325b834555e4
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-01-27 00:10:26 +0100

    Reduce block search range

--- src/Targets/BeNearBlock.ts
+++ src/Targets/BeNearBlock.ts
@@ -4,7 +4,7 @@ import Target from "./Target";
 import WalkTo from "../Actions/WalkTo";
 
 const COMPLETED_DISTANCE = 3;
-const SEARCH_DISTANCE = 528;
+const SEARCH_DISTANCE = 256;
 
 export default class BeNearBlock implements Target {
   private blockName: string;
@@ -34,7 +34,8 @@ export default class BeNearBlock implements Target {
 
     if (!nearestBlock) {
       // ToDo - Place block
-      
+      console.log(`No block found: ${this.blockName}`);
+
       return [];
     }
 
