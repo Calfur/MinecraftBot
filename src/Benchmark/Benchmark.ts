f81a1fba74921ea176ae171242538b303e8bfb32
TobiasgutPrivat <tobias.gut.03@gmail.com>
2025-03-03 13:53:39 +0100

    track runs

--- src/Benchmark/Benchmark.ts
+++ src/Benchmark/Benchmark.ts
@@ -19,28 +19,30 @@ export default class BenchMark {
         steve.bot.once('spawn', () => {
             steve.bot.chat('/clear ' + steve.bot.username);
             steve.neededActions.push(...this.targets);
-            const log: BenchRun = new BenchRun();
+            const run: BenchRun = new BenchRun();
+
+            this.runs.push(run);
 
             const startTime = Date.now();
         
             steve.on("factorChanged", (id: string, value: any) => {
-                log.factors.push(new State(Date.now() - startTime, id, value));
+                run.factors.push(new State(Date.now() - startTime, id, value));
             })
             
             steve.on("event", (id: string, reason: string) => {
-                log.state.push(new Event(Date.now() - startTime, id, reason))
+                run.state.push(new Event(Date.now() - startTime, id, reason))
             })
 
             steve.once('finishedActions', () => {
                 console.log(`Finished actions in ${Date.now() - startTime}ms`);
-                log.state.push(new Event(Date.now() - startTime, "end", "finished actions"));
-                log.save(this.name);
+                run.state.push(new Event(Date.now() - startTime, "end", "finished actions"));
+                run.save(this.name);
             });
 
             steve.once('end', () => {
                 console.log(`cancelled at ${Date.now() - startTime}ms`);
-                log.state.push(new Event(Date.now() - startTime, "end", "cancelled"));
-                log.save(this.name);
+                run.state.push(new Event(Date.now() - startTime, "end", "cancelled"));
+                run.save(this.name);
             });
         });
     }
